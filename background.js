e632bf3053d0d088615cb95e0b7d50cbbf012049
onuraykac@gmail.com <yineParam9213#>
2015-06-17 17:16:25 +0300

    selected word'de protocol varsa yanlış çalışıyordu. giderildi.

--- background.js
+++ background.js
@@ -1,13 +1,31 @@
 chrome.extension.onMessage.addListener(function(request) {
-  var isWebAddress = isThatAWebSite(request.url);
+  var isWebAddress = isThatAWebAddress(request.url);
   if (isWebAddress) {
-    chrome.tabs.create({ url: "http://" + request.url });
+    var protocol = existProtocolHandlers(request.url);
+    if(protocol){
+      chrome.tabs.create({ url: request.url });
+    }else{
+      chrome.tabs.create({ url: "http://" + request.url });
+    }
   }else{
     chrome.tabs.create({ url: "http://www.google.com/?#q=" + request.url });
   }
-
 });
 
-function isThatAWebSite(url){
-  return url.match(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/);
-}
+function isThatAWebAddress(url){
+    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+    if (url.match(regex)){
+      return true;
+    } else {
+      return false;
+    }
+};
+
+function existProtocolHandlers(url){
+    if (url.indexOf("http://")==0 || url.indexOf("https://")==0) {
+        return true;
+    }else{
+      return false;
+    }
+};
