aa749ed0adb971f514ffddc27accb95d69299819
onuraykac@gmail.com <yineParam9213#>
2015-07-28 16:04:09 +0300

    - code formatting.
    - openSameTab("<"), openNewTabInBackground("ctrl+shift+<") özellikleri eklendi.

--- background.js
+++ background.js
@@ -2,30 +2,45 @@ chrome.extension.onMessage.addListener(function(request) {
   var isWebAddress = isThatAWebAddress(request.url);
   if (isWebAddress) {
     var protocol = existProtocolHandlers(request.url);
-    if(protocol){
-      chrome.tabs.create({ url: request.url });
-    }else{
-      chrome.tabs.create({ url: "http://" + request.url });
+    var url;
+    if (protocol) {
+      url = request.url;
+    } else {
+      url = "http://" + request.url;
     }
-  }else{
-    chrome.tabs.create({ url: "http://www.google.com/?#q=" + request.url });
+  } else {
+    url = "http://www.google.com/?#q=" + request.url;
   }
+  openTab(url, request.newTab, request.selectTab);
 });
 
-function isThatAWebAddress(url){
-    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-    var regex = new RegExp(expression);
-    if (url.match(regex)){
-      return true;
-    } else {
-      return false;
-    }
+function openTab(url, newTab, selectTab) {
+  if (newTab) {
+    chrome.tabs.create({
+      url: url,
+      selected: selectTab
+    });
+  } else {
+    chrome.tabs.update({
+      url: url
+    });
+  }
+}
+
+function isThatAWebAddress(url) {
+  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  var regex = new RegExp(expression);
+  if (url.match(regex)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-function existProtocolHandlers(url){
-    if (url.indexOf("http://")==0 || url.indexOf("https://")==0) {
-        return true;
-    }else{
-      return false;
-    }
+function existProtocolHandlers(url) {
+  if (url.indexOf("http://") == 0 || url.indexOf("https://") == 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
--- content.js
+++ content.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
-  $("body").keyup(function(e){
-    if(e.which !== 226){
-      return;
-    }
-
-    var selection = window.getSelection();
-    var text = selection.toString().trim();
-    if(text){
-      chrome.extension.sendMessage( { url: text } );
+  $("body").keyup(function(e) {
+    if (e.which === 226) {
+      selection = window.getSelection();
+      var text = selection.toString().trim();
+      if (text) {
+        var newTab = e.ctrlKey;
+        var selectTab = !e.shiftKey;
+        chrome.extension.sendMessage({
+          url: text,
+          newTab: newTab,
+          selectTab: selectTab
+        });
+      }
     }
   });
 });
